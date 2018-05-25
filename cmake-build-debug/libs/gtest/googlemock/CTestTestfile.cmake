10d25adfdc66d3de26bb5e3e7e94831e8b2ca575
Alex <alexbatashev@gmail.com>
2018-05-25 20:51:24 +0300

    remove cmake-debug-build

--- cmake-build-debug/libs/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aleksandrbatasev/CLionProjects/athena/libs/gtest/googlemock
-# Build directory: /Users/aleksandrbatasev/CLionProjects/athena/cmake-build-debug/libs/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- cmake-build-debug/libs/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aleksandrbatasev/CLionProjects/athena/libs/gtest/googletest
-# Build directory: /Users/aleksandrbatasev/CLionProjects/athena/cmake-build-debug/libs/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
