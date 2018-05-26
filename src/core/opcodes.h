480c60c37499e592519d20c1b4f138d038ac654e
Alex <alexbatashev@gmail.com>
2018-05-26 09:06:51 +0300

    add some docs

--- src/core/opcodes.h
+++ src/core/opcodes.h
@@ -5,10 +5,10 @@
 #ifndef ATHENA_OPCODES_H
 #define ATHENA_OPCODES_H
 enum class OpCode : uint32_t {
-    DEL,
-    PUSH,
-    POP,
-    JMP,
-    ADD
+    DEL = 0x00,
+    PUSH = 0x01,
+    POP = 0x02,
+    JMP = 0x03,
+    ADD = 0x04
 };
 #endif //ATHENA_OPCODES_H
