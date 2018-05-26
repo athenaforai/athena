b09673950feaf3b4706b946b0b2420f9e1c94165
Alex <alexbatashev@gmail.com>
2018-05-26 23:29:13 +0300

    add

--- src/backend/generic/add.cpp
+++ src/backend/generic/add.cpp
@@ -9,9 +9,26 @@
 #endif
 
 namespace athena::backend::generic {
-    void add(athena::core::Tensor* a, athena::core::Tensor* b) {
-        if (a->getShape() != b->getShape()) {
-            throw std::runtime_error("Can't add two tensors with different dimensions");
+
+    athena::core::Tensor* addf_1d(athena::core::Tensor* a, athena::core::Tensor* b) {
+#ifdef __APPLE__
+        auto y = new float[b->getShape().dim(0)];
+        memcpy(y, b->raw(), b->getShape().dim(0));
+
+        catlas_saxpby(a->getShape().dim(0), 1.0f, reinterpret_cast<float *>(a->raw()), 1, 1.0f, y, 1);
+#endif
+        return nullptr;
+    }
+
+    athena::core::Tensor* add(athena::core::Tensor* a, athena::core::Tensor* b) {
+//        if (a->getShape() != b->getShape()) {
+//            throw std::runtime_error("Can't add two tensors with different dimensions");
+//        }
+
+        // assume equal sizes for now
+        if (a->getShape().dimensions() == 1) {
+            return addf_1d(a, b);
         }
+
     }
 }
