42055864b649c3adbb808f53fcd0b062947ba196
Alex <alexbatashev@gmail.com>
2018-07-31 17:12:37 +0300

    delete comments

--- src/backend/generic/GenericMemoryManager.cpp
+++ src/backend/generic/GenericMemoryManager.cpp
@@ -5,7 +5,7 @@
  * Licensed under MIT license.
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+ * distributed under the License is distributed on an “AS IS” BASIS, WITHOUTщд
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
