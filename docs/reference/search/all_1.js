dd5a97989f015c311023675e639c6f113e453c7e
Alex <alexbatashev@gmail.com>
2018-08-02 17:09:58 +0300

    resolve conflicts

--- docs/reference/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cpudevice',['CPUDevice',['../db/d7b/classathena_1_1backend_1_1generic_1_1_c_p_u_device.html',1,'athena::backend::generic']]]
-];
--- docs/reference/search/all_10.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['unlock',['unlock',['../d2/df1/classathena_1_1backend_1_1_abstract_memory_manager.html#aec859ee3bf6011d8710b2ec4bfc2373e',1,'athena::backend::AbstractMemoryManager::unlock()'],['../d6/d21/classathena_1_1backend_1_1generic_1_1_generic_memory_manager.html#a58a2b56a07a96c3cabb7b8fd079b3eae',1,'athena::backend::generic::GenericMemoryManager::unlock()']]]
-];
