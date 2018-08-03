e6386b48f1a0b397a670eaf167aeb0dc0e8c915b
Alex <alexbatashev@gmail.com>
2018-08-03 14:00:50 +0300

    reduce required RAM size, remove unused flags

--- src/backend/generic/CPUDevice.cpp
+++ src/backend/generic/CPUDevice.cpp
@@ -17,7 +17,7 @@
 athena::backend::generic::CPUDevice::CPUDevice () : AbstractDevice() {
     maxThreads = std::thread::hardware_concurrency();
     activeThreads = 0;
-    memorySize = 1024 * 1024 * 1024; // 1 GB todo replace with real mem size
+    memorySize = 1024 * 1024; // 1 MB todo replace with real mem size
     memoryManager = new GenericMemoryManager();
     memoryManager->setMemSize( memorySize );
     memoryManager->init();
